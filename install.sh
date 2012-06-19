13bcba1625a06664c918bc06f437d4afa1aee081
Brandon McCaig <bamccaig@gmail.com>
2012-06-19 10:02:24 -0400

    Add a template offlineimap.conf file.
    Add it to install.sh's ignored files for now since it is a template and
    shouldn't be used yet.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
 all_files=(`ls -d .*`);
-ignored_files=(. .. .git .gitignore);
+ignored_files=(. .. .git .gitignore offlineimap.conf);
 
 function resolve_rc_path
 {
