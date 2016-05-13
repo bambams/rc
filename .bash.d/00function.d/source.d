864839674d23c138e3d833bf235cdc11d61b4acd
Brandon McCaig <bamccaig@gmail.com>
2016-05-13 17:01:35 -0400

    h4x: bash.d fixes for Git Bash environment.
    That seems to get these things working properly, but alas I am still not
    able to use Git Bash (in Windows) until I figure out a proper Perl cpanm
    config so that I can install packages (e.g., File::Next, App::Ack,
    dependencies...).

--- .bash.d/00function.d/source.d
+++ .bash.d/00function.d/source.d
@@ -1,7 +1,9 @@
 function source.d
 {
+    local name="";
+
     if [ ! -z "$1" ]; then
-        local name=".$1";
+        name=".$1";
     fi;
 
     for f in `find -L "$HOME/.bash.d$name" -type f | sort`; do
